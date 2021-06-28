dce083ece15dd8a4100dc7d8ee90ee87251d5c23
Nikolaj <nh@itstack.dk>
2021-06-28 15:58:40 +0200

    Reorder checkout - WIP

---
+++ wp-content/themes/storefront-child-theme-master/woocommerce/checkout/form-shipping.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Checkout shipping information form
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/form-shipping.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.6.0
+ * @global WC_Checkout $checkout
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<div class="woocommerce-shipping-fields">
+	<?php if ( true === WC()->cart->needs_shipping_address() ) : ?>
+
+		<h3 id="ship-to-different-address">
+			<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
+				<input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php esc_html_e( 'Ship to a different address?', 'woocommerce' ); ?></span>
+			</label>
+		</h3>
+
+		<div class="shipping_address">
+
+			<?php do_action( 'woocommerce_before_checkout_shipping_form', $checkout ); ?>
+
+			<div class="woocommerce-shipping-fields__field-wrapper">
+				<?php
+				$fields = $checkout->get_checkout_fields( 'shipping' );
+
+				foreach ( $fields as $key => $field ) {
+					woocommerce_form_field( $key, $field, $checkout->get_value( $key ) );
+				}
+				?>
+			</div>
+
+			<?php do_action( 'woocommerce_after_checkout_shipping_form', $checkout ); ?>
+
+		</div>
+
+	<?php endif; ?>
+</div>
+
+<div class="woocommerce_choose_shipping">
+
+	<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+
+	<?php do_action( 'woocommerce_review_order_before_shipping' ); ?>
+
+	<?php wc_cart_totals_shipping_html(); ?>
+
+	<?php do_action( 'woocommerce_review_order_after_shipping' ); ?>
+
+	<?php endif; ?>
+		
+</div>
+
+
+<!-- 
+<div class="woocommerce-additional-fields">
+	<?php // do_action( 'woocommerce_before_order_notes', $checkout ); ?>
+
+	<?php //  if ( apply_filters( 'woocommerce_enable_order_notes_field', 'yes' === get_option( 'woocommerce_enable_order_comments', 'yes' ) ) ) : ?>
+
+		<?php // if ( ! WC()->cart->needs_shipping() || wc_ship_to_billing_address_only() ) : ?>
+
+			<h3><?php // esc_html_e( 'Additional information', 'woocommerce' ); ?></h3>
+
+		<?php //endif; ?>
+
+		<div class="woocommerce-additional-fields__field-wrapper">
+			<?php // foreach ( $checkout->get_checkout_fields( 'order' ) as $key => $field ) : ?>
+				<?php // woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+			<?php // endforeach; ?>
+		</div>
+
+	<?php // endif; ?>
+
+	<?php // do_action( 'woocommerce_after_order_notes', $checkout ); ?>
+</div>
+ -->
+
+<?php
+ if ( ! is_ajax() ) {
+	do_action( 'woocommerce_review_order_before_payment' );
+}
+?>
+<div id="payment" class="woocommerce-checkout-payment">
+	<?php if ( WC()->cart->needs_payment() ) : ?>
+		<ul class="wc_payment_methods payment_methods methods">
+			<?php
+			if ( ! empty( $available_gateways ) ) {
+				foreach ( $available_gateways as $gateway ) {
+					wc_get_template( 'checkout/payment-method.php', array( 'gateway' => $gateway ) );
+				}
+			} else {
+				echo '<li class="woocommerce-notice woocommerce-notice--info woocommerce-info">' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</li>'; // @codingStandardsIgnoreLine
+			}
+			?>
+		</ul>
+	<?php endif; ?>
+	<div class="form-row place-order">
+		<noscript>
+			<?php
+			/* translators: $1 and $2 opening and closing emphasis tags respectively */
+			printf( esc_html__( 'Since your browser does not support JavaScript, or it is disabled, please ensure you click the %1$sUpdate Totals%2$s button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce' ), '<em>', '</em>' );
+			?>
+			<br/><button type="submit" class="button alt" name="woocommerce_checkout_update_totals" value="<?php esc_attr_e( 'Update totals', 'woocommerce' ); ?>"><?php esc_html_e( 'Update totals', 'woocommerce' ); ?></button>
+		</noscript>
+
+		<?php wc_get_template( 'checkout/terms.php' ); ?>
+
+		<?php do_action( 'woocommerce_review_order_before_submit' ); ?>
+
+		<?php echo apply_filters( 'woocommerce_order_button_html', '<button type="submit" class="button alt" name="woocommerce_checkout_place_order" id="place_order" value="' . esc_attr( $order_button_text ) . '" data-value="' . esc_attr( $order_button_text ) . '">' . esc_html( $order_button_text ) . '</button>' ); // @codingStandardsIgnoreLine ?>
+
+		<?php do_action( 'woocommerce_review_order_after_submit' ); ?>
+
+		<?php wp_nonce_field( 'woocommerce-process_checkout', 'woocommerce-process-checkout-nonce' ); ?>
+	</div>
+</div>
+<?php
+if ( ! is_ajax() ) {
+	do_action( 'woocommerce_review_order_after_payment' );
+}
---
+++ wp-content/themes/storefront-child-theme-master/woocommerce/checkout/review-order.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Review order table
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/review-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.8.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+<!--
+<table class="shop_table woocommerce-checkout-review-order-table">
+	<thead>
+		<tr>
+			<th class="product-name"><?php // esc_html_e( 'Product', 'woocommerce' ); ?></th>
+			<th class="product-total"><?php // esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+		</tr>
+	</thead>
+	<tbody> -->
+		<?php
+/* 		do_action( 'woocommerce_review_order_before_cart_contents' );
+
+		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+
+			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+				?>
+				<tr class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+					<td class="product-name">
+						<?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</td>
+					<td class="product-total">
+						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</td>
+				</tr>
+				<?php
+			}
+		}
+
+		do_action( 'woocommerce_review_order_after_cart_contents' ); */
+		?>
+	<!-- </tbody>
+	<tfoot>
+
+		<tr class="cart-subtotal">
+			<th><?php // esc_html_e( 'Subtotal', 'woocommerce' ); ?></th>
+			<td><?php // wc_cart_totals_subtotal_html(); ?></td>
+		</tr> -->
+
+		<?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
+			<tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+				<th><?php wc_cart_totals_coupon_label( $coupon ); ?></th>
+				<td><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+		<?php // if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+
+			<?php // do_action( 'woocommerce_review_order_before_shipping' ); ?>
+
+			<?php // wc_cart_totals_shipping_html(); ?>
+
+			<?php // do_action( 'woocommerce_review_order_after_shipping' ); ?>
+
+		<?php // endif; ?>
+
+		<?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
+			<tr class="fee">
+				<th><?php echo esc_html( $fee->name ); ?></th>
+				<td><?php wc_cart_totals_fee_html( $fee ); ?></td>
+			</tr>
+		<?php endforeach; ?>
+
+		<?php if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) : ?>
+			<?php if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
+				<?php // foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited ?>
+					<tr class="tax-rate tax-rate-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
+						<th><?php echo esc_html( $tax->label ); ?></th>
+						<td><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
+					</tr>
+				<?php // endforeach; ?>
+			<?php else : ?>
+				<tr class="tax-total">
+					<th><?php // echo esc_html( WC()->countries->tax_or_vat() ); ?></th>
+					<td><?php // wc_cart_totals_taxes_total_html(); ?></td>
+				</tr>
+			<?php endif; ?>
+		<?php endif; ?>
+
+		<?php // do_action( 'woocommerce_review_order_before_order_total' ); ?>
+
+		<!-- <tr class="order-total">
+			<th><?php // esc_html_e( 'Total', 'woocommerce' ); ?></th>
+			<td><?php // wc_cart_totals_order_total_html(); ?></td>
+		</tr> -->
+
+		<?php // do_action( 'woocommerce_review_order_after_order_total' ); ?>
+
+<!-- 	</tfoot>
+</table> -->
+
+
+
+<div class="shop_table woocommerce-checkout-review-order-table">
+
+
+	<div class="order-total">
+		<div><?php esc_html_e( 'Total', 'woocommerce' ); ?></div>
+		<div><?php wc_cart_totals_order_total_html(); ?></div>
+	</div>
+
+
+	<div class="cart-subtotal">
+		<div><?php esc_html_e( 'Subtotal', 'woocommerce' ); ?></div>
+		<div><?php wc_cart_totals_subtotal_html(); ?></div>
+	</div>
+
+
+	<!-- Products -->
+	<?php
+		do_action( 'woocommerce_review_order_before_cart_contents' );
+
+		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+			$_product = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
+
+			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
+				?>
+				<div class="<?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
+					<div class="product-name">
+						<?php echo "<p>" . apply_filters( 'woocommerce_cart_item_name', $_product->get_name(), $cart_item, $cart_item_key ) . '&nbsp;' . "</p>"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+
+					<div class="product-quantity">
+						<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <span>Antal: <strong class="product-quantity">' . sprintf( '%s', $cart_item['quantity'] ) . '</strong></span>', $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			        </div>
+
+					<div clas="product-variable">
+						<?php echo wc_get_formatted_cart_item_data( $cart_item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+
+					<div class="product-price">
+						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</div>
+			</div>
+				<?php
+			}
+		}
+
+		do_action( 'woocommerce_review_order_after_cart_contents' );
+		?>
+
+
+		<!-- Additional fields -->
+		<div class="woocommerce-additional-fields">
+			<?php do_action( 'woocommerce_before_order_notes', $checkout ); ?>
+
+			<?php if ( apply_filters( 'woocommerce_enable_order_notes_field', 'yes' === get_option( 'woocommerce_enable_order_comments', 'yes' ) ) ) : ?>
+
+				<?php if ( ! WC()->cart->needs_shipping() || wc_ship_to_billing_address_only() ) : ?>
+
+					<h3><?php esc_html_e( 'Additional information', 'woocommerce' ); ?></h3>
+
+				<?php endif; ?>
+
+				<div class="woocommerce-additional-fields__field-wrapper">
+					<?php foreach ( $checkout->get_checkout_fields( 'order' ) as $key => $field ) : ?>
+						<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+					<?php endforeach; ?>
+				</div>
+
+			<?php endif; ?>
+
+			<?php do_action( 'woocommerce_after_order_notes', $checkout ); ?>
+		</div>
+
+
+</div>
