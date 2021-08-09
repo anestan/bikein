3a775302478c8db12f600923beedcbb6801f58ae
Nikolaj <nh@itstack.dk>
2021-08-09 12:08:16 +0200

    Updated JS for progress bar

--- wp-content/themes/storefront-child-theme-master/js/progress_bar.js
+++ wp-content/themes/storefront-child-theme-master/js/progress_bar.js
@@ -16,10 +16,20 @@ jQuery(function($){
 			success : function( data ){
 				if( data ) {
 			
-					// replace value with post count * 2
+					// replace value with post count + post count
 					var post_count = loadmore_params.post_count;
 					$('#progress-bar').val(post_count + post_count);
-				
+
+					var post_found = loadmore_params.post_found;
+					var post_count_dobble = post_count * 2;
+
+					if ( post_count_dobble > post_found ) {
+						$('.woocommerce-result-count ').replaceWith('<p class="woocommerce-result-count">Viser ' + (post_count_dobble - 1 )  + ' af ' + post_found + ' resultater</p>' );
+					}
+					else {
+						$('.woocommerce-result-count ').replaceWith('<p class="woocommerce-result-count">Viser ' + post_count_dobble + ' af ' + post_found + ' resultater</p>' );
+					}
+
 				}
 			}
 		});
