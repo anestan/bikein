118926b8da9900a30df0f3c0c5f084766c264eab
Martin <mk@itstack.dk>
2021-08-11 14:40:08 +0200

    Archive styling + Filter and category compressed

--- wp-content/themes/storefront-child-theme-master/js/filters.js
+++ wp-content/themes/storefront-child-theme-master/js/filters.js
@@ -1,7 +1,15 @@
 //Show or hide archive filters
 jQuery(function($){
 	$(".show_filter").click(function() {
-	  $("#archive-top-filters").slideToggle(400);
+	  $("#archive-top-filters").slideToggle(300);
 		$("#archive-top-filters").css("display","flex");
+	});
+
+	$(".show_sidebar_filter").click(function() {
+	  $("#secondary .widget_layered_nav").slideToggle(300);
+	});
+
+	$(".show_sidebar_cat").click(function() {
+	  $("#secondary .widget_product_categories").slideToggle(300);
 	});
 });
