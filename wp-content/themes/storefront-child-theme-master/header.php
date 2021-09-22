25d94d3702464844d4383082f383b37e00bff276
Martin <mk@itstack.dk>
2021-09-22 10:39:25 +0200

    Cart preview

--- wp-content/themes/storefront-child-theme-master/header.php
+++ wp-content/themes/storefront-child-theme-master/header.php
@@ -111,6 +111,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 							?>
 						</div>
 					</a>
+					<?php include 'inc/woocommerce/cart-preview.php';?>
 	    <!-- Mobil menu -->
 			    <div class="header-mobile">
 						<a href="#menu"><span><i class="fas fa-bars"></i></span></a>
