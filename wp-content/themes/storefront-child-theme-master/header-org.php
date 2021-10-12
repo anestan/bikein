4c5cf18c3d69abefe876f82b58c3fa78832ac257
Martin <mk@itstack.dk>
2021-10-12 11:56:27 +0200

    Switched to original header

--- wp-content/themes/storefront-child-theme-master/header-org.php
+++ wp-content/themes/storefront-child-theme-master/header-org.php
@@ -99,28 +99,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			      <?php echo do_shortcode('[fibosearch]'); ?>
 			    </div>
 
-					<!-- Navigation -->
-					<div class="navigation-header-content">
-					<!-- Menu -->
-						<div class="storefront-primary-navigation">
-							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_html_e( 'Primary Navigation', 'storefront' ); ?>">
-								  <div class="menu">
-								  <?php
-								  wp_nav_menu(
-								    array(
-								      'theme_location'  => 'primary',
-								      'container_class' => 'primary-navigation',
-								    )
-								  );
-								  ?>
-								</div>
-							</nav><!-- #site-navigation -->
-						</div>
-					</div> <!-- End navigation header -->
-
 			    <!-- Cart -->
-					<a class="header-cart-wrapper" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>">
-						<div class="header-cart">
+					<div class="header-cart-wrapper">
+						<a class="header-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>">
 							<div class="cart-product-amount">
 								<?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?>
 							</div>
@@ -128,8 +109,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 							<?php
 								// echo do_shortcode( '[elementor-template id="10350"]' );
 							?>
-						</div>
-					</a>
+						</a>
+					</div>
+					<?php include 'inc/woocommerce/cart-preview.php';?>
 	    <!-- Mobil menu -->
 			    <div class="header-mobile">
 						<a href="#menu"><span><i class="fas fa-bars"></i></span></a>
@@ -170,6 +152,23 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					</script>
 		    </div> <!-- End Header Wrapper -->
 			</div>
+			<div class="navigation-header-content">
+			<!-- Menu -->
+				<div class="storefront-primary-navigation">
+					<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_html_e( 'Primary Navigation', 'storefront' ); ?>">
+						  <div class="menu">
+						  <?php
+						  wp_nav_menu(
+						    array(
+						      'theme_location'  => 'primary',
+						      'container_class' => 'primary-navigation',
+						    )
+						  );
+						  ?>
+						</div>
+					</nav><!-- #site-navigation -->
+				</div>
+			</div> <!-- End navigation header -->
 			<?php
 			/**
 			 * Functions hooked into storefront_header action
