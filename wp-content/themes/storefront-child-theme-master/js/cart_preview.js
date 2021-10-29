9303a88eb7b851da20be481015a53182c74c14b7
Martin <mk@itstack.dk>
2021-10-29 12:57:33 +0200

    Cart checkout styling + variables.scss

--- wp-content/themes/storefront-child-theme-master/js/cart_preview.js
+++ wp-content/themes/storefront-child-theme-master/js/cart_preview.js
@@ -1,14 +1,17 @@
 jQuery(function($){
-  var timeout;
-     //Show and hide cart preview on hover
-     $(".header-cart, #cart-preview").hover(function(){
-         clearTimeout(timeout);
-         $('#cart-preview').slideDown(300);
-     },function(){
-       timeout = setTimeout(function () {
-         $('#cart-preview').hide();
-       }, 600);
-     });
- });
- 
- 
+ var timeout;
+
+    if (!$('body').hasClass('woocommerce-checkout') &&
+        !$('body').hasClass('woocommerce-cart')) {
+
+      //Show and hide cart preview on hover
+      $(".header-cart, #cart-preview").hover(function(){
+          clearTimeout(timeout);
+          $('#cart-preview').slideDown(300);
+      },function(){
+        timeout = setTimeout(function () {
+          $('#cart-preview').hide();
+        }, 600);
+      });
+    }
+});
